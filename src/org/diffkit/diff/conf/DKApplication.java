37b429e14cb3f6371e92380154b42dfe5349f510
smart_zhen <wangzhen@ats-oa.com>
2016-06-30 11:43:18 +0800

    加上可以传递参数 在args里 带上 -extraParam_key value

--- src/org/diffkit/diff/conf/DKApplication.java
+++ src/org/diffkit/diff/conf/DKApplication.java
@@ -67,7 +67,7 @@ public class DKApplication {
    private static final String PLAN_FILE_OPTION_KEY = "planfiles";
    private static final String ERROR_ON_DIFF_OPTION_KEY = "errorOnDiff";
    private static final String DEMO_DB_OPTION_KEY = "demoDB";
-   private static final String USER_EXTRA_PARAM_KEY = "extraParam.key";
+   private static final String USER_EXTRA_PARAM_KEY = "extraParam_key";
    private static final Options OPTIONS = new Options();
 
    private static final String LOGBACK_FILE_NAME = "logback.xml";
